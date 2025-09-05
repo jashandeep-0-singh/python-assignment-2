1286a386075852ba69243b7fb7a4d8ca3f7b8a93
jashandeep-0-singh <138032511+jashandeep-0-singh@users.noreply.github.com>
2025-09-05 11:38:17 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,9 @@
+value = int(input("enter your number :- "))
+55
+if value % 2== 0 :
+    print(f"the given number {value} is an even ")
+
+else :
+    print(f"the given number {value} is an odd")
+
+print("thank you for using...")
---
+++ task 2.py
@@ -0,0 +1,6 @@
+a = 0 
+
+for i in range (1,51):
+    a += i
+
+print(f"Your sum for 1 to 50 is \"{a}\".")
