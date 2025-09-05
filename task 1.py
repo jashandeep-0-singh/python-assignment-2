2741d29e75e4656527185d7a168ba8953097d7bc
jashandeep-0-singh <138032511+jashandeep-0-singh@users.noreply.github.com>
2025-09-05 11:41:54 +0530

    task 1.py

--- task 1.py
+++ task 1.py
@@ -1,9 +1,10 @@
 value = int(input("enter your number :- "))
-55
+
 if value % 2== 0 :
     print(f"the given number {value} is an even ")
 
 else :
     print(f"the given number {value} is an odd")
+
 
 print("thank you for using...")
